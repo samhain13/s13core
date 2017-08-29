16f7ef6febd9c794448f6c65f38c190189b7f590
Arielle B Cruz <arelle.cruz@gmail.com>
2017-08-29 15:17:04 +0800

    removed Django admin templates; added s13core admin to apps

--- website/settings/production.py
+++ website/settings/production.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     's13core',
+    's13core.administration',
     's13core.content_management',
     's13core.settings',
 ]
@@ -33,20 +34,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
-    { # Used for the Django Admin Site.
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-    { # Used for everything else.
+    {
         'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
